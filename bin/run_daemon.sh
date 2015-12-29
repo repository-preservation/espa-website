018d52a41475afbea0332727c4ba79c7379ca7ff
clay austin <cgaustin@usgs.gov>
2015-12-29 17:01:51 +0000

    Enable inclusion of custom apache config file in Docker container. The Docker framework doesn't allow conditionals for managing which functions get run when building an image, so this is managed by putting that logic in a wrapper script with builds the image, and then launches the container

--- bin/run_daemon.sh
+++ bin/run_daemon.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-cd ../
+if [ ! -f Dockerfile ]
+  then
+    echo "Dockerfile not found, moving up a directory.."
+    cd ../
+    if [ ! -f Dockerfile ]
+      then
+        echo "Dockerfile not found. This script should"
+        echo "be called as bin/run_daemon.sh or ./run_daemon.sh"
+        echo "from the espa-website project folder"
+        echo "exiting..." 
+        exit 1
+    fi
+fi 
+
+if [ -f httpd.conf.espa ]
+  then
+    echo "custom Apache config found, enabling in Dockerfile.."
+    sed -i "s/^# RUN mv/RUN mv/g" Dockerfile
+    sed -i "s/^# COPY/COPY/g" Dockerfile
+fi
 
 echo "Building image..."
 docker build -t espa-apache2 .
